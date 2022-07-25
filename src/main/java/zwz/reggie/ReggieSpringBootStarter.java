dedaca4b93d4bffb24447605138d332773bd31b8
jvlin <zwz@163.com>
2022-07-25 11:04:00 +0800

    sencond commit filter

--- src/main/java/zwz/reggie/ReggieSpringBootStarter.java
+++ src/main/java/zwz/reggie/ReggieSpringBootStarter.java
@@ -4,7 +4,9 @@ package zwz.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @Slf4j
 @SpringBootApplication
 public class ReggieSpringBootStarter {
